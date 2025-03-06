4581d1cc63b0ddf7382d8d1b5639d38b18f1a04c
lorena <lorena.ramos@itscordoba.edu.ar>
2025-03-06 17:30:21 -0300

    modificado

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
 import './styles/Global.css'; 
-=======
-import './style/Global.css'; // Asegúrate que el path sea correcto
-
-
->>>>>>> b7a754c1d37919254ea0be37137b68c5d839ce43
 import App from './App';
 
 
