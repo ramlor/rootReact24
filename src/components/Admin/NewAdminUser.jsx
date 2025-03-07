cb978c4adf60593f5123474b533b43fa00ffca84
Yoha <cpoteljunot@hotmail.com>
2025-03-07 02:48:22 -0300

    cambios en userban.

--- src/components/Admin/NewAdminUser.jsx
+++ src/components/Admin/NewAdminUser.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../../styles/Global.css';
 
-
-
+//
 const NewAdminUser = () => {
     const [name, setName] = useState('');
     const [lastName, setLastName] = useState('');
